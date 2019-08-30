aaee384b9220d6dce395b3622b4d17e6da2cc0c6
haiphamhong <48507551+haiphamhong@users.noreply.github.com>
2019-08-30 15:23:34 +0700

    Update rs-test1.cpp

--- rs-test1.cpp
+++ rs-test1.cpp
@@ -13,16 +13,16 @@ vector<Point2f> distanceframe(rs2::depth_frame depth, int width, int height);
 
 int main(int argc, char * argv[]) try
 {
-    int w, h;
+    int w = 640;
+    int h = 360;
     rs2::align align_to(RS2_STREAM_DEPTH);
     // Declare depth colorizer for pretty visualization of depth data
     rs2::colorizer color_map;
     rs2::hole_filling_filter hole;
     hole.set_option(RS2_OPTION_HOLES_FILL,1);
     rs2::config cfg;
-    cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 0);
-    cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 0);
-    cfg.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 0);
+    cfg.enable_stream(RS2_STREAM_COLOR, w, h, RS2_FORMAT_BGR8, 0);
+    cfg.enable_stream(RS2_STREAM_DEPTH, w, h, RS2_FORMAT_Z16, 0);
     // Declare RealSense pipeline, encapsulating the actual device and sensors
     rs2::pipeline pipe;
     // Start streaming with default recommended configuration
@@ -32,51 +32,32 @@ int main(int argc, char * argv[]) try
     const auto window_name = "Display Image";
     namedWindow(window_name, WINDOW_AUTOSIZE);
     
-    //VideoWriter video("rs-depVideo2.avi",CV_FOURCC('M','J','P','G'),10, Size(640,480)); 
-
     while (waitKey(1) < 0 && getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
     {
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
-        rs2::frameset data1 = data;
-        data1 = data1.apply_filter(hole);
-        data1 = data1.apply_filter(align_to);
+        rs2::depth_frame depth = data.get_depth_frame();
+        data = data.apply_filter(hole);
+        data = data.apply_filter(align_to);
 
         //rs2::frame imageColor = data.get_color_frame();
         //rs2::frame ir_frame = data.first(RS2_STREAM_INFRARED);
         
-        rs2::frame dep_frame = data1.get_color_frame();
-        rs2::depth_frame depth = data1.get_depth_frame();
+        rs2::frame dep_frame = data.get_color_frame();
         
-
-        // Query frame size (width and height)
-        w = depth.as<rs2::video_frame>().get_width();
-        h = depth.as<rs2::video_frame>().get_height();
         
-        //cout<<"width: "<<w<<endl;
-        //cout<<"height: "<<h<<endl;
         // Create OpenCV matrix of size (w,h) from the colorized depth data
-        //Mat image1(Size(w, h), CV_8UC3, (void*)imageColor.get_data(), Mat::AUTO_STEP);
-        //Mat image2(Size(w, h), CV_8UC1, (void*)ir_frame.get_data(), Mat::AUTO_STEP);
-        Mat image3(Size(w, h), CV_8UC3, (void*)dep_frame.get_data(), Mat::AUTO_STEP);
-
+        Mat image(Size(w, h), CV_8UC3, (void*)dep_frame.get_data(), Mat::AUTO_STEP);
         
-        vector<Point2f> hai = distanceframe(depth, w, h);
-        for(int i = 0; i < 4; i++ )
-        line(image3, hai[i], hai[(i+1)%4], Scalar(0, 255, 0), 1, LINE_AA);
-        circle(image3, Point2f(w/2, h/2), 3, Scalar(0, 0, 255), -1);
-        circle(image3, Point2f(w-40, 20), 3, Scalar(0, 0, 255), -1);
-        //Mat colorhai;
-        //cvtColor(hai, colorhai, CV_GRAY2BGR);
-        //video.write(colorhai);
-
-        //mau.create(image3.size(), image3.type());
-        //mau = Scalar::all(0);
-        //image3.copyTo(mau, hai);
+        vector<Point2f> hai = getCorner(depth, w, h);
+        cout<<"size corner: "<<hai.size()<<endl;
+        if( hai.size() > 1)
+        {
+            for(int i = 0; i < hai.size()-1; i++ )
+            line(image, hai[i], hai[(i+1)%4], Scalar(0, 255, 0), 1, LINE_AA);
+        }
+        
         // Update the window with new data
-        imshow(window_name, image3);
-        //imshow("frame depth", hai);
-        //imshow("mau", mau);
-        //waitKey(0);
+        imshow(window_name, image);
     }
 
     return EXIT_SUCCESS;
