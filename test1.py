7ba9122660c20d2879509218e2ae7d8ef15a9fd5
daothanlong11 <1611853@hcmut.edu.vn>
2019-09-20 01:26:57 +0700

    final

--- test1.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-img = cv2.imread("D:\\Study\\Nam3\\intern\\stereo_camera.jpg")
-size = img.shape
-new_img = cv2.resize(img,(size[0],size[1]))
-cv2.imwrite("D:\\Study\\Nam3\\intern\\new_stereo_camera.jpg")
